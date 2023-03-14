18b580b649f1526c7f0dd3a2777e350a92c4d918
thaolaptrinh <thaolaptrinh@gmail.com>
2023-03-14 17:04:57 +0700

    update style

--- pystravel-vn/assets/js/main.js
+++ pystravel-vn/assets/js/main.js
@@ -1,4 +1,4 @@
-$(".owl-carousel").owlCarousel({
+$(".partner .owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
   dots: false,
@@ -18,6 +18,28 @@ $(".owl-carousel").owlCarousel({
     },
     1000: {
       items: 7,
+    },
+  },
+});
+
+$(".slider .owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  dots: false,
+  nav: true,
+  navText: [
+    "<i class='fa fa-arrow-circle-left' aria-hidden='true'></i>",
+    "<i class='fa fa-arrow-circle-right' aria-hidden='true'></i>",
+    ,
+  ],
+
+  responsive: {
+    0: {
+      items: 1,
+    },
+
+    1000: {
+      items: 2,
     },
   },
 });
